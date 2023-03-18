7ebdae685158c5c2058e360efd0df3e8526af617
RimuruChan <RealSprite233@outlook.com>
2023-03-18 22:16:04 +0800

    repo: add preloader

---
+++ packages/p/preloader/xmake.lua
@@ -0,0 +1,10 @@
+package("preloader")
+    set_description("The preloader package")
+
+    add_urls("https://github.com/LiteLDev/PreLoader/releases/download/$(version)/preloader-$(version)-windows-amd64.zip")
+    add_versions("1.0", "099d3dcd1c8d111041028d0a49c085020ae8ae53da63db05e90d15182684fce9")
+
+    on_install(function (package)
+        os.cp("include/*", package:installdir("include"))
+        os.cp("PreLoader.lib", package:installdir("lib"))
+    end)
