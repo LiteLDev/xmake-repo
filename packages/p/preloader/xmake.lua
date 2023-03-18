0337355a2ae7801d1723af515a64c23e901c605c
RimuruChan <RealSprite233@outlook.com>
2023-03-18 22:23:17 +0800

    repo: fix preloader

--- packages/p/preloader/xmake.lua
+++ packages/p/preloader/xmake.lua
@@ -1,8 +1,8 @@
 package("preloader")
     set_description("The preloader package")
 
-    add_urls("https://github.com/LiteLDev/PreLoader/releases/download/$(version)/preloader-$(version)-windows-amd64.zip")
-    add_versions("v1.0.0", "099d3dcd1c8d111041028d0a49c085020ae8ae53da63db05e90d15182684fce9")
+    add_urls("https://github.com/LiteLDev/PreLoader/releases/download/v$(version)/preloader-$(version)-windows-amd64.zip")
+    add_versions("1.0.0", "099d3dcd1c8d111041028d0a49c085020ae8ae53da63db05e90d15182684fce9")
 
     on_install(function (package)
         os.cp("include/*", package:installdir("include"))
