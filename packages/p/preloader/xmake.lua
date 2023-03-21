027c3fe8bd5d95a2273220bfeed72279122d297a
RimuruChan <RealSprite233@outlook.com>
2023-03-21 08:28:23 +0800

    repo: bump preloader to v1.0.2

--- packages/p/preloader/xmake.lua
+++ packages/p/preloader/xmake.lua
@@ -2,7 +2,7 @@ package("preloader")
     set_description("The preloader package")
 
     add_urls("https://github.com/LiteLDev/PreLoader/releases/download/$(version)/preloader-$(version)-windows-amd64.zip")
-    add_versions("v1.0.1", "d4cab43d6a98f44fe1a58b46ecd0dbab606dd961918f3cede6af9b33b4a76dba")
+    add_versions("v1.0.2", "9868557faa4668d5b9dbff58ba1f43a409064d6b982f156704503680f1eb43a4")
 
     on_install(function (package)
         os.cp("include/*", package:installdir("include"))
