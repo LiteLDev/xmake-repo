99014bb3dcb067278cdc8d46759691238007f25b
RimuruChan <RealSprite233@outlook.com>
2023-03-22 22:35:39 +0800

    repo: update preloader

--- packages/p/preloader/xmake.lua
+++ packages/p/preloader/xmake.lua
@@ -1,10 +1,15 @@
 package("preloader")
     set_description("The preloader package")
 
-    add_urls("https://github.com/LiteLDev/PreLoader/releases/download/$(version)/preloader-$(version)-windows-amd64.zip")
-    add_versions("v1.0.2", "9868557faa4668d5b9dbff58ba1f43a409064d6b982f156704503680f1eb43a4")
+    add_urls("https://github.com/LiteLDev/PreLoader.git")
+    add_versions("v1.0.3", "9ec6cd9c2e0590e02229037435a15036eedd925a")
+
+    add_configs("shared", {description = "Build shared library.", default = true, type = "boolean", readonly = true})
 
     on_install(function (package)
-        os.cp("include/*", package:installdir("include"))
-        os.cp("PreLoader.lib", package:installdir("lib"))
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
     end)
