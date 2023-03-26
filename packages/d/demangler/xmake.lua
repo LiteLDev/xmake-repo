80af52cd31173a554a66bc5d8df240cc9f693f72
RimuruChan <RealSprite233@outlook.com>
2023-03-26 23:40:35 +0800

    repo: update demangler

--- packages/d/demangler/xmake.lua
+++ packages/d/demangler/xmake.lua
@@ -2,7 +2,9 @@ package("demangler")
     set_description("The demangler package")
 
     add_urls("https://github.com/LiteLDev/Demangler.git")
+
     add_versions("v1.0.2", "6218c355e07abc197862fc2c8c3d9eeea8462c75")
+    add_versions("v2.0.0", "88865df2816654aa5e61d1646f69e02c97cd60b0")
 
     on_install(function (package)
         local configs = {}
