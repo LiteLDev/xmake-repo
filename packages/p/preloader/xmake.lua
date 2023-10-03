fafae1efa1393e2411a0c921c0ee308becdfa26d
RimuruChan <42002296+RimuruChan@users.noreply.github.com>
2023-10-03 23:22:21 +0800

    repo: update preloader

--- packages/p/preloader/xmake.lua
+++ packages/p/preloader/xmake.lua
@@ -4,6 +4,7 @@ package("preloader")
     add_urls("https://github.com/LiteLDev/PreLoader.git")
     add_versions("v1.0.3", "9ec6cd9c2e0590e02229037435a15036eedd925a")
     add_versions("v1.1.0", "0b54d73ade8e534c390fae5451e3e541b74c184e")
+    add_versions("v1.2.0", "9fa4a932867b51bf50193838e47944e70175212c")
 
     add_configs("shared", {description = "Build shared library.", default = true, type = "boolean", readonly = true})
 
