89cebfbf5d206b3c39628a4fee076c1be56510eb
RimuruChan <RealSprite233@outlook.com>
2023-10-10 23:42:08 +0800

    repo: update preloader

--- packages/p/preloader/xmake.lua
+++ packages/p/preloader/xmake.lua
@@ -5,6 +5,7 @@ package("preloader")
     add_versions("v1.0.3", "9ec6cd9c2e0590e02229037435a15036eedd925a")
     add_versions("v1.1.0", "0b54d73ade8e534c390fae5451e3e541b74c184e")
     add_versions("v1.2.0", "9fa4a932867b51bf50193838e47944e70175212c")
+    add_versions("v1.3.0", "1007284479673a1b14560dfbd94a92f8645777f3")
 
     add_configs("shared", {description = "Build shared library.", default = true, type = "boolean", readonly = true})
 
