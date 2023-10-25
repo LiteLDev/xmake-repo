b408e391155a9c10a88c0620855b60ccf00b0a96
OEOTYAN <58554322+OEOTYAN@users.noreply.github.com>
2023-10-26 05:48:21 +0800

    repo: update pfr

--- packages/p/pfr/xmake.lua
+++ packages/p/pfr/xmake.lua
@@ -2,7 +2,7 @@ package("pfr")
 
     set_description("std::tuple like methods for user defined types without any macro or boilerplate code")
 
-    set_urls("https://github.com/LiteLDev/pfr.git")
+    set_urls("https://github.com/boostorg/pfr.git")
     add_versions("2.1.1", "294a4976bd04829dd204aaf9e9fd30338a5d3199")
 
     on_install(function (package)
