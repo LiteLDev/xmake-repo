56f827ece94ebec13299627fe3101ecf3fdf2602
Zijian Zhang <futrime@outlook.com>
2023-10-29 17:14:02 +0800

    repo: move ScriptX to LiteLDev/ScriptX

--- packages/s/scriptx/xmake.lua
+++ packages/s/scriptx/xmake.lua
@@ -1,5 +1,5 @@
 package("scriptx")
-    add_urls("https://github.com/LiteLDev/prebuilt-scriptx/releases/download/$(version)/scriptx-windows-amd64.zip")
+    add_urls("https://github.com/LiteLDev/ScriptX/releases/download/$(version)/scriptx-windows-x64.zip")
     add_versions("v0.1.0", "c7d5e9d87101528db81b3d1f8b441848b21b19fb3ca873831cb567ff59fdfd0a")
 
     add_configs("backend", {default = '8', values = {"lua", "python", "quickjs", "v8"}})
