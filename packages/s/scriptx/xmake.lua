c9ce195a0c02e2aa11fb40ca54c3cc43d4c3a5f5
Zijian Zhang <futrime@outlook.com>
2023-10-29 16:01:42 +0800

    repo: add ScriptX backend selector

--- packages/s/scriptx/xmake.lua
+++ packages/s/scriptx/xmake.lua
@@ -2,7 +2,10 @@ package("scriptx")
     add_urls("https://github.com/LiteLDev/prebuilt-scriptx/releases/download/$(version)/scriptx-windows-amd64.zip")
     add_versions("v0.1.0", "c7d5e9d87101528db81b3d1f8b441848b21b19fb3ca873831cb567ff59fdfd0a")
 
+    add_config("backend", {values = {"lua", "python", "quickjs", "v8"}})
+
     on_install(function (package)
+        local backend = package:config("backend")
         os.cp("include/*", package:installdir("include", "scriptx"))
-        os.cp("lib/*", package:installdir("lib"))
+        os.cp("lib/scriptx_" .. backend, package:installdir("lib"))
     end)
