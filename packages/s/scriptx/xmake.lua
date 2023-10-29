619c2f0095c6304a9464edd45e46d57ca4114eb3
Zijian Zhang <futrime@outlook.com>
2023-10-29 15:55:40 +0800

    repo: update hash for scriptx

--- packages/s/scriptx/xmake.lua
+++ packages/s/scriptx/xmake.lua
@@ -1,6 +1,6 @@
 package("scriptx")
     add_urls("https://github.com/LiteLDev/prebuilt-scriptx/releases/download/$(version)/scriptx-windows-amd64.zip")
-    add_versions("v0.1.0", "1ac7f92cd9597c44b37e84d315adfb30915602e03d8e2e93d22578af8f8589dc")
+    add_versions("v0.1.0", "c7d5e9d87101528db81b3d1f8b441848b21b19fb3ca873831cb567ff59fdfd0a")
 
     on_install(function (package)
         os.cp("include/*", package:installdir("include", "scriptx"))
