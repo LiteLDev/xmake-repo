7a12033d4c22fa96ad3c9c12b74ba2d288e449da
Futrime <futrime@outlook.com>
2023-11-22 10:35:23 +0800

    repo: add LeviLamina 0.1.0

--- packages/l/levilamina/xmake.lua
+++ packages/l/levilamina/xmake.lua
@@ -1,13 +1,10 @@
 package("levilamina")
     set_description("A lightweight, modular and versatile plugin loader for Minecraft Bedrock Server BDS, formerly known as LiteLoaderBDS")
 
-    add_urls("https://github.com/LiteLDev/LeviLamina.git")
-    add_versions("v1.0.0", "494f858bf808dd43a2d103e851aa5c69023d2e0f")
+    add_urls("https://github.com/LiteLDev/LeviLamina/releases/download/$(version)/levilamina-windows-x64.zip")
+    add_versions("v0.1.0", "fdf3e6fbd688bd97bf21c9a372df24af08cfd9b2619e495bcada290134f52d4f")
 
     on_install(function (package)
-        local configs = {}
-        if package:config("shared") then
-            configs.kind = "shared"
-        end
-        import("package.tools.xmake").install(package, configs)
+        os.cp("include", package:installdir())
+        os.cp("lib/*.lib", package:installdir("lib"))
     end)
