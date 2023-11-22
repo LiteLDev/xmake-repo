bbf6e6f17189a88c23c823a90620aec0b47f2298
Zijian Zhang <futrime@outlook.com>
2023-11-23 00:00:18 +0800

    repo: update levilamina

--- packages/l/levilamina/xmake.lua
+++ packages/l/levilamina/xmake.lua
@@ -1,8 +1,20 @@
 package("levilamina")
-    set_description("A lightweight, modular and versatile plugin loader for Minecraft Bedrock Server BDS, formerly known as LiteLoaderBDS")
-
     add_urls("https://github.com/LiteLDev/LeviLamina/releases/download/v$(version)/levilamina-windows-x64.zip")
     add_versions("0.1.0", "bc4ee0cd9cbb59a3e57031b150ad990c17a7355474dfefac6ad3e05ef785bcd3")
+
+    on_load(function (package)
+        local DEPS = {
+            ["0.1.0"] = {
+                "fmt 10.1.1",
+                "magic_enum 0.9.0",
+                "nlohmann_json 3.11.2"
+            }
+        }
+
+        for _, dep in ipairs(DEPS[package:version_str()]) do
+            package:add("deps", dep)
+        end
+    end)
 
     on_install(function (package)
         os.cp("include", package:installdir())
