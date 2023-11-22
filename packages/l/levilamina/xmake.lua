09eb8be58a06c8cea0dd75e74bb8a21a1b18ad15
Zijian Zhang <futrime@outlook.com>
2023-11-22 18:33:10 +0800

    repo: fix typo

--- packages/l/levilamina/xmake.lua
+++ packages/l/levilamina/xmake.lua
@@ -1,8 +1,8 @@
 package("levilamina")
     set_description("A lightweight, modular and versatile plugin loader for Minecraft Bedrock Server BDS, formerly known as LiteLoaderBDS")
 
-    add_urls("https://github.com/LiteLDev/LeviLamina/releases/download/$(version)/levilamina-windows-x64.zip")
-    add_versions("v0.1.0", "bc4ee0cd9cbb59a3e57031b150ad990c17a7355474dfefac6ad3e05ef785bcd3")
+    add_urls("https://github.com/LiteLDev/LeviLamina/releases/download/v$(version)/levilamina-windows-x64.zip")
+    add_versions("0.1.0", "bc4ee0cd9cbb59a3e57031b150ad990c17a7355474dfefac6ad3e05ef785bcd3")
 
     on_install(function (package)
         os.cp("include", package:installdir())
