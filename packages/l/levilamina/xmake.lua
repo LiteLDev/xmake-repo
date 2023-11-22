8b7b7b9e73dc68e0b13d73c10d6383bc61bb328d
Zijian Zhang <futrime@outlook.com>
2023-11-23 07:42:38 +0800

    repo(levilamina): fix pfr

--- packages/l/levilamina/xmake.lua
+++ packages/l/levilamina/xmake.lua
@@ -3,12 +3,12 @@ package("levilamina")
     add_versions("0.1.0", "bc4ee0cd9cbb59a3e57031b150ad990c17a7355474dfefac6ad3e05ef785bcd3")
 
     on_load(function (package)
-        local DEPS = {
-            ["0.1.0"] = {
-                "fmt 10.1.1",
-                "magic_enum 0.9.0",
-                "nlohmann_json 3.11.2"
-            }
+        local DEPS = {}
+        DEPS["0.1.0"] = {
+            "fmt 10.1.1",
+            "magic_enum 0.9.0",
+            "nlohmann_json 3.11.2",
+            "pfr 2.1.1"
         }
 
         for _, dep in ipairs(DEPS[package:version_str()]) do
