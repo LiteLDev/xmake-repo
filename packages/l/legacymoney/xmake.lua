f6c10633655ecf1119bc02c7bffb61dd391381e4
ShrBox <shrbox@outlook.com>
2024-01-13 11:38:36 +0800

    repo: update legacymoney

--- packages/l/legacymoney/xmake.lua
+++ packages/l/legacymoney/xmake.lua
@@ -2,6 +2,7 @@ package("legacymoney")
     add_urls("https://github.com/LiteLDev/LegacyMoney/releases/download/v$(version)/LegacyMoney-windows-x64.zip")
     add_versions("0.1.0", "ea33c23bc116cec686fbae3187c3fecece0c1dd8b208cdeef3b101b5a5fdc412")
     add_versions("0.1.2", "08d86510ad41411a365a0c4bd65fce96aae3455dbc408703d57a9752d0c9552e")
+    add_versions("0.1.3", "9f356f8e9742b198638ec2f42aa1fc2fd0d158225d31eb14fdef14858577c369")
 
     on_install(function (package)
         os.cp("include", package:installdir())
