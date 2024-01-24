0ef4d5d6fd8d5414a38b4517396b8585b99d62c1
NahidaChan <42824603+KawaiiNahida@users.noreply.github.com>
2024-01-24 23:05:11 +0800

    dep: add demangler v17.0.6

--- packages/d/demangler/xmake.lua
+++ packages/d/demangler/xmake.lua
@@ -3,8 +3,9 @@ package("demangler")
 
     add_urls("https://github.com/LiteLDev/Demangler.git")
 
-    add_versions("v1.0.2", "6218c355e07abc197862fc2c8c3d9eeea8462c75")
-    add_versions("v2.0.0", "88865df2816654aa5e61d1646f69e02c97cd60b0")
+    add_versions("v1.0.2",  "6218c355e07abc197862fc2c8c3d9eeea8462c75")
+    add_versions("v2.0.0",  "88865df2816654aa5e61d1646f69e02c97cd60b0")
+    add_versions("v17.0.6", "94d8cd7a51ddd432e56ca1aba0fc091a48a10dff")
 
     on_install(function (package)
         local configs = {}
