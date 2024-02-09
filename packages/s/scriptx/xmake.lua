35bc1eac5b709417822c2108ac156be7cad1b7c8
Futrime <futrime@outlook.com>
2024-02-09 10:38:07 +0800

    repo(scriptx): create

---
+++ packages/s/scriptx/xmake.lua
@@ -0,0 +1,30 @@
+package("scriptx")
+    add_urls("https://github.com/LiteLDev/ScriptX.git")
+    add_versions("3.2.0", "1fae62097926328b7dd7e5415344e9d7fda465b4")
+
+    add_configs("backend", {default = "Lua", values = {"Lua", "QuickJs"}})
+    add_includedirs(
+        "include/src/include/"
+    )
+
+    on_install(function (package)
+        local configs = {
+            backend = package:config("backend"),
+        }
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_load(function (package)
+        local backend = package:config("backend")
+
+        local deps = {
+            Lua = "lua v5.4.6",
+            QuickJs = "quickjs 2022-03-07",
+        }
+
+        print("Using ScriptX config: backend=" .. backend)
+    
+        package:add("defines", "SCRIPTX_BACKEND=" .. backend)
+        package:add("defines", "SCRIPTX_BACKEND_TRAIT_PREFIX=../backend/" .. backend .. "/trait/Trait")
+        package:add("deps", deps[backend])
+    end)
