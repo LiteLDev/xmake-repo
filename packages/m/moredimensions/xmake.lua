80b6d64c78e59e2e0f6f8abc9366befc48720fa3
Qiuzhizhe <42761326+quizhizhe@users.noreply.github.com>
2024-03-08 07:18:30 -0800

    repo(moredimensions): fix error name

--- packages/m/moredimensions/xmake.lua
+++ packages/m/moredimensions/xmake.lua
@@ -1,5 +1,5 @@
-package("moredimension")
-    add_urls("https://github.com/LiteLDev/MoreDimension.git")
+package("moredimensions")
+    add_urls("https://github.com/LiteLDev/MoreDimensions.git")
     add_versions("0.1.0", "cbda295e273f675c84f6f25a0d4e8b9fb1b70608")
 
     add_deps("levilamina 0.8.3")
