1c6909f841513847e54a4679a22f67ae6b5c4cd5
ShrBox <shrbox@outlook.com>
2024-03-08 20:28:38 +0800

    repo: submit node

---
+++ packages/n/node/xmake.lua
@@ -0,0 +1,8 @@
+package("node")
+    add_urls("https://github.com/LiteLDev/node/releases/download/$(version)/node-win-x86_64.zip")
+    add_versions("v16.16.0", "9b04418f18692c296afb3d4da8bfd0ce8794bfb371a7289c2cd00536cc516bee")
+
+    add_includedirs("include/v8/")
+    on_install(function (package)
+        os.cp("*", package:installdir())
+    end)
--- packages/q/quickjs/xmake.lua
+++ packages/q/quickjs/xmake.lua
@@ -1,5 +1,5 @@
 package("quickjs")
-    add_urls("https://github.com/LiteLDev/quickjs/releases/download/2022-03-07/quickjs-win-x86_64.zip")
+    add_urls("https://github.com/LiteLDev/quickjs/releases/download/$(version)/quickjs-win-x86_64.zip")
     add_versions("2022-03-07", "af0c38b0cf80aa1deb58e727e408477fffcc6f5f57da537dffc335861d652ed0")
 
     on_install(function (package)
