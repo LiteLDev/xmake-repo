5e3d3978d62f78dd8d811a1d6a17254063eba295
Qiuzhizhe <42761326+quizhizhe@users.noreply.github.com>
2024-03-08 23:05:42 -0800

    Update xmake.lua

--- packages/m/moredimensions/xmake.lua
+++ packages/m/moredimensions/xmake.lua
@@ -2,7 +2,7 @@ package("moredimensions")
     add_urls("https://github.com/LiteLDev/MoreDimensions.git")
     add_versions("0.1.0", "cbda295e273f675c84f6f25a0d4e8b9fb1b70608")
 
-    add_deps("levilamina 0.8.3")
+    add_deps("levilamina")
     on_install(function (package)
         import("package.tools.xmake").install(package)
     end)
