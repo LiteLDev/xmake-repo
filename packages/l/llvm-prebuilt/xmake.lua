6ea7493848f3ce8aa4836aac641ccb8c61bc9498
OEOTYAN <58554322+OEOTYAN@users.noreply.github.com>
2024-03-11 07:25:09 +0800

    chore: change llvm-prebuilt source

--- packages/l/llvm-prebuilt/xmake.lua
+++ packages/l/llvm-prebuilt/xmake.lua
@@ -2,9 +2,10 @@ package("llvm-prebuilt")
     set_homepage("https://llvm.org/")
     set_description("The LLVM Project is a collection of modular and reusable compiler and toolchain technologies.")
     set_license("Apache-2.0")
-    add_urls("https://github.com/awakecoding/llvm-prebuilt/releases/download/v2023.3.0/clang+llvm-$(version)-x86_64-windows.tar.xz")
+    add_urls("https://github.com/LiteLDev/llvm-prebuilt/releases/download/v$(version)/clang+llvm-x86_64-windows.tar.xz")
 
-    add_versions("16.0.6", "0d1c9f11572a382e62fd15d8937d37465795eb7bb87beecb0827d3668f201563")
+    add_versions("16.0.6", "AC4B0CA88FCF156017C0115F3FB253C33672778DEBDB6513CFCA2133783E36C0")
+    add_versions("18.1.1", "62F750BFCEFC78CFD43C6E9282CE7621C97E33123F53930F82086CF7CCB612AB")
 
     on_install("windows", function (package)
         if not is_arch("x64") then
