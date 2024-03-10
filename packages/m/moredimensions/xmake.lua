b9a906bee3aec3c5d4838d1319884af189b885cc
Qiuzhizhe <42761326+quizhizhe@users.noreply.github.com>
2024-03-10 08:32:57 -0700

    repo(moredimensions): add v0.1.2

--- packages/m/moredimensions/xmake.lua
+++ packages/m/moredimensions/xmake.lua
@@ -2,6 +2,7 @@ package("moredimensions")
     add_urls("https://github.com/LiteLDev/MoreDimensions.git")
     add_versions("0.1.0", "cbda295e273f675c84f6f25a0d4e8b9fb1b70608")
     add_versions("0.1.1", "2b078626f60b5371aa9ed726cde48c9dabd829f8")
+    add_versions("0.1.2", "2e7d9aa2c34587434acb052fd25b73dcc1a3b26bb")
 
     add_deps("levilamina")
     on_install(function (package)
