edcf3fc5652697c5f0955b63897caa9ab633928e
OEOTYAN <58554322+OEOTYAN@users.noreply.github.com>
2024-03-12 19:16:26 +0800

    chore: update url of llvm-prebuilt

--- packages/l/llvm-prebuilt/xmake.lua
+++ packages/l/llvm-prebuilt/xmake.lua
@@ -2,10 +2,9 @@ package("llvm-prebuilt")
     set_homepage("https://llvm.org/")
     set_description("The LLVM Project is a collection of modular and reusable compiler and toolchain technologies.")
     set_license("Apache-2.0")
-    add_urls("https://github.com/LiteLDev/llvm-prebuilt/releases/download/v$(version)/clang+llvm-x86_64-windows.tar.xz")
+    add_urls("https://github.com/llvm/llvm-project/releases/download/llvmorg-v$(version)/clang+llvm-$(version)-x86_64-pc-windows-msvc.tar.xz")
 
-    add_versions("16.0.6", "AC4B0CA88FCF156017C0115F3FB253C33672778DEBDB6513CFCA2133783E36C0")
-    add_versions("18.1.1", "84D0FFB5067D6BBB801D045939AD0C0D27EECEAEF8C8AE5A855F3460400809A9")
+    add_versions("18.1.1", "79ea242c0fbd66c632ed3aaebf6f821c1e4c03140497c67ea750443eb36bfc5d")
 
     on_install("windows", function (package)
         if not is_arch("x64") then
