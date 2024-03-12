1ca98614f5a2435cd906e98c8034fcc8150b0551
OEOTYAN <58554322+OEOTYAN@users.noreply.github.com>
2024-03-13 01:03:22 +0800

    fix: use lower case sha

--- packages/l/llvm-prebuilt/xmake.lua
+++ packages/l/llvm-prebuilt/xmake.lua
@@ -4,7 +4,7 @@ package("llvm-prebuilt")
     set_license("Apache-2.0")
     add_urls("https://github.com/LiteLDev/llvm-prebuilt/releases/download/v$(version)/clang+llvm-$(version)-x86_64-windows.tar.xz")
 
-    add_versions("18.1.1", "EF183114540CB2219A3EBC9BD2020CF7E7F207CCA3B3A07E74BF692681F51B12")
+    add_versions("18.1.1", "ef183114540cb2219a3ebc9bd2020cf7e7f207cca3b3a07e74bf692681f51b12")
 
     on_install("windows", function (package)
         if not is_arch("x64") then
