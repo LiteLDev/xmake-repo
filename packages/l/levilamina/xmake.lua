ebe3a1ace9bd09d6288171a6f346966d53a93b57
RimuruChan <RealSprite233@outlook.com>
2024-03-18 00:46:07 +0800

    repo: bump levilamina to v0.9.5

--- packages/l/levilamina/xmake.lua
+++ packages/l/levilamina/xmake.lua
@@ -7,6 +7,7 @@ package("levilamina")
     add_versions("0.9.2", "2f58b1be95bac2b87170e6fab5a5aae9dea69225")
     add_versions("0.9.3", "890cb5c2b86dd9498853e6813854ad99e6f849ea")
     add_versions("0.9.4", "bc8b5dc8f1ef84dc44f58cc44c6e86cc21b788b8")
+    add_versions("0.9.5", "bc8b5dc8f1ef84dc44f58cc44c6e86cc21b788b8")
 
     local dependencies = {
         ["0.8.3"] = {"bdslibrary 1.20.61.01"},
@@ -16,6 +17,7 @@ package("levilamina")
         ["0.9.2"] = {"bdslibrary 1.20.62.02"},
         ["0.9.3"] = {"bdslibrary 1.20.62.02"},
         ["0.9.4"] = {"bdslibrary 1.20.62.02"}
+        ["0.9.5"] = {"bdslibrary 1.20.62.02"}
     }
 
     add_deps("entt")
