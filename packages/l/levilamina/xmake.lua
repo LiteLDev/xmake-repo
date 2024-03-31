0c02b35df5f97acd678afb90a2f28320b9dc7285
ShrBox <shrbox@outlook.com>
2024-03-31 21:00:43 +0800

    revert: "repo(levilamina): add 0.10.3"
    This reverts commit a5df349d9f2359d47d008bf272489bb16d04aa1d.

--- packages/l/levilamina/xmake.lua
+++ packages/l/levilamina/xmake.lua
@@ -11,7 +11,6 @@ package("levilamina")
     add_versions("0.10.0", "c93593bad49a24846159a892b5e5960af8aa8cf9")
     add_versions("0.10.1", "a142aa8c6c5df9d25ab90879d394c46f239f6aee")
     add_versions("0.10.2", "c5acae6cb20b5fff23f1018f8293a938a2e178c7")
-    add_versions("0.10.3", "0782af1d1414c0557232b682d7e782d2eefc808e")
 
     local dependencies = {
         ["0.8.3"] = {"bdslibrary 1.20.61.01"},
@@ -24,8 +23,7 @@ package("levilamina")
         ["0.9.5"] = {"bdslibrary 1.20.62.02"},
         ["0.10.0"] = {"bdslibrary 1.20.72.01"},
         ["0.10.1"] = {"bdslibrary 1.20.72.01"},
-        ["0.10.2"] = {"bdslibrary 1.20.72.01"},
-        ["0.10.3"] = {"bdslibrary 1.20.72.01"}
+        ["0.10.2"] = {"bdslibrary 1.20.72.01"}
     }
 
     add_deps("entt")
