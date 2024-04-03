936986d64f91a072bd9b681a88296ca50f62e14d
OEOTYAN <58554322+OEOTYAN@users.noreply.github.com>
2024-04-03 22:40:48 +0800

    repo: update bsci

--- packages/b/bsci/xmake.lua
+++ packages/b/bsci/xmake.lua
@@ -1,6 +1,7 @@
 package("bsci")
     add_urls("https://github.com/OEOTYAN/BedrockServerClientInterface.git")
     add_versions("0.1.0", "bde86a823bc3c191e65a5139551de0d2cb4a0b90")
+    add_versions("0.1.1", "29f0adee9f5cc58cf0e9f818ac04bcb5ec0d5dc1")
 
     add_deps("levilamina")
     on_install(function (package)
