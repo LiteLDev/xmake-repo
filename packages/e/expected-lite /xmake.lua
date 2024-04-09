8d6c83f7e452b2d9c42b5889740cac7a38ba3500
OEOTYAN <58554322+OEOTYAN@users.noreply.github.com>
2024-04-09 21:34:59 +0800

    repo: add expected-lite

---
+++ packages/e/expected-lite /xmake.lua
@@ -0,0 +1,22 @@
+package("expected-lite")
+    set_kind("library", {headeronly = true})
+    set_homepage("https://github.com/martinmoene/expected-lite")
+    set_description("expected lite - Expected objects in C++11 and later in a single-file header-only library")
+    set_license("BSL-1.0")
+
+    add_urls("https://github.com/martinmoene/expected-lite.git")
+
+    add_versions("v0.7.0", "3634b0a6d8dffcffad4d1355253d79290c0c754c")
+
+    on_install(function (package)
+        os.cp("include", package:installdir())
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include <nonstd/expected.hpp>
+            nonstd::expected<int, std::string> test() {
+                return 0;
+            }
+        ]]}, {configs = {languages = "c++11"}}))
+    end)
