f45901f92386eb485a394f691f5fc3e7b599469b
ShrBox <shrbox@outlook.com>
2024-04-13 23:42:11 +0800

    repo(moredimension): refactor xmake.lua

--- packages/m/moredimensions/xmake.lua
+++ packages/m/moredimensions/xmake.lua
@@ -4,8 +4,27 @@ package("moredimensions")
     add_versions("0.1.1", "2b078626f60b5371aa9ed726cde48c9dabd829f8")
     add_versions("0.1.2", "e7d9aa2c34587434acb052fd25b73dcc1a3b26bb")
     add_versions("0.2.0", "eb2f733b21f258c2e8925db7517e81b7818d3740")
+    add_versions("0.2.1", "82b58e1fea4c024b5db62c4e00665f24ef3df9c3")
 
-    add_deps("levilamina")
+    local dependencies = {
+        ["0.1.x"] = {"levilamina 0.9.5"},
+        ["0.2.0"] = {"levilamina 0.10.5"},
+        ["0.2.1"] = {"levilamina 0.11.1"}
+    }
+
+    on_load(function (package)
+        import("core.base.semver")
+        for key, value in pairs(dependencies) do
+            try{function()
+                if semver.satisfies(package:version_str(), key) then
+                    for _, dependency in ipairs(value) do
+                        package:add("deps", dependency)
+                    end
+                end
+            end}
+        end
+    end)
+
     on_install(function (package)
         import("package.tools.xmake").install(package)
     end)
