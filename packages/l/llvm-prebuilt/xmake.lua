879bda9b720a5331a7e0446c7144fa71e67e10c1
OEOTYAN <58554322+OEOTYAN@users.noreply.github.com>
2024-04-26 12:33:36 +0800

    repo: update llvm-prebuilt

--- packages/l/llvm-prebuilt/xmake.lua
+++ packages/l/llvm-prebuilt/xmake.lua
@@ -4,7 +4,9 @@ package("llvm-prebuilt")
     set_license("Apache-2.0")
     add_urls("https://github.com/LiteLDev/llvm-prebuilt/releases/download/v$(version)/clang+llvm-$(version)-x86_64-windows.tar.xz")
 
+    add_versions("16.0.6", "0ac1b63a8be705074e910bef347e407aef38c3c11572dfe4c097cda19e9937a0")
     add_versions("18.1.1", "ef183114540cb2219a3ebc9bd2020cf7e7f207cca3b3a07e74bf692681f51b12")
+    add_versions("18.1.4", "b49dc54698fd209981768988a6e705f814cb8db7dcfc1398d651749fde686b02")
     if is_plat("windows") then
         add_syslinks("version", "wsock32")
     end
