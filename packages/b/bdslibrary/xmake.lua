3034b228c6ff5b2d227f11ac2c874889f5667e15
qingyu <166664660@qq.com>
2024-04-30 00:28:33 +0800

    repo(bdslibrary): add version 1.20.81.01

--- packages/b/bdslibrary/xmake.lua
+++ packages/b/bdslibrary/xmake.lua
@@ -2,6 +2,7 @@ package("bdslibrary")
     set_description("The bdslibrary package")
 
     add_urls("https://github.com/LiteLDev/BDSLibrary/releases/download/$(version)/bdslibrary-$(version)-windows-amd64.zip")
+    add_versions("1.20.81.01", "c0459683035536b6bc3113906dcf85601f5fc757609347296dd0d7f127ee5fa7")
     add_versions("1.21.0.23", "2669264b678e5fced924c5a0d7bf0d7902c33c620d92471dc276bd6d2c107b5c")
     add_versions("1.20.80.05", "8da6c2dbe04b156525e1e0a0c286e8cadd4efcc08c3e50d7e716397e4a4576fe")
     add_versions("1.21.0.20", "a56ec88eba78da39145a7047d1ec539b042d6848b5bb9ed5ffd9734739f072a6")
