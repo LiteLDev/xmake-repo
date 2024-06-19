bd6756856e22133532ad3b0fdff225e8782393c8
ShrBox <shrbox@outlook.com>
2024-06-19 11:03:50 +0800

    repo: update legacy plugins to 0.8.1

--- packages/l/legacymoney/xmake.lua
+++ packages/l/legacymoney/xmake.lua
@@ -6,7 +6,7 @@ package("legacymoney")
     add_versions("0.5.0", "ee27b89450f30a2d4af09a2b53cc498a4d2e4399cc30038467a67cc6f1c06cdf")
     add_versions("0.6.0", "f985e973a7a8cdcb736d2ecd14dce8abbea76c1abe25c269661545ed52838400")
     add_versions("0.7.0", "6784da5c3ecf04e7a225c62283b6adf7a69566b01a1b7023d82913efb92203e3")
-    add_versions("0.8.0", "d8f567d02d5d40d0d69ba2e6f31200998aa443164096ac4b119b6844337cbe39")
+    add_versions("0.8.1", "f3d0713078335b8412c04e7287ae87f565492ea47b6db44131fe56ada6ece02f")
 
     on_install(function (package)
         os.cp("include", package:installdir())
