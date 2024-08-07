3186506c90805d4deb992727dc90fdf47e4b1c58
OEOTYAN <OEOTYAN@outlook.com>
2024-08-08 06:03:26 +0800

    refactor: move rules to new repo

--- packages/l/levibuildscript/xmake.lua
+++ packages/l/levibuildscript/xmake.lua
@@ -1,4 +1,7 @@
 package("levibuildscript")
     add_urls("https://github.com/LiteLDev/LeviBuildScript.git")
     set_kind("binary")
-    add_versions("0.1.0", "68bc0c7aee69048938b53989121acbaa95294f65")
+    add_versions("0.1.0", "c114f86f40f7fc980deaff8c3e55f642278dd6f0")
+    on_install(function (package)
+        os.mv("rules/*", package:installdir("rules"))
+    end)
