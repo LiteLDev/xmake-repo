4d4e4d05b4d77cbd3f324165b5cc9b0be50ce265
OEOTYAN <OEOTYAN@outlook.com>
2024-08-08 04:30:04 +0800

    refactor: rename linkrule

--- packages/b/bedrockdata/rules/linkrule.lua
+++ packages/b/bedrockdata/rules/linkrule.lua
@@ -1,16 +1,12 @@
-rule("bdswin")
+rule("linkrule")
     before_link(function (target)
         import("lib.detect.find_file")
         os.mkdir("$(buildir)/.prelink/lib")
-        local args = {}
-        for i, obj in ipairs(target:objectfiles()) do
-            args[i] = path.absolute(obj)
-        end
+
         local data = assert(find_file("bedrock_runtime_data", {"$(env PATH)"}), "Cannot find bedrock_runtime_data")
         local link = assert(find_file("prelink.exe", {"$(env PATH)"}), "Cannot find prelink.exe")
 
-        args = table.join({vformat("$(buildir)/.prelink"), path.absolute(data)}, args)
-        os.runv(link, args)
+        os.runv(link, table.join({vformat("$(buildir)/.prelink"), data}, target:objectfiles()))
         target:add("linkdirs", "$(buildir)/.prelink/lib")
         target:add("shflags", "bedrock_server_api.lib", {force = true})
         target:add("shflags", "bedrock_server_var.lib", {force = true})
