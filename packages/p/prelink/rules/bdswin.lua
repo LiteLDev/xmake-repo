1d241f8823befe2d06a3997455bc6fdc5584cf9c
OEOTYAN <OEOTYAN@outlook.com>
2024-08-08 03:47:06 +0800

    fix: fix prelink rules

--- packages/p/prelink/rules/bdswin.lua
+++ packages/p/prelink/rules/bdswin.lua
@@ -2,14 +2,14 @@ rule("bdswin")
     before_link(function (target)
         import("lib.detect.find_file")
         os.mkdir("$(buildir)/.prelink/lib")
-        args = {}
+        local args = {}
         for i, obj in ipairs(target:objectfiles()) do
             args[i] = path.absolute(obj)
         end
         local data = assert(find_file("bedrock_runtime_data", {"$(env PATH)"}), "Cannot find bedrock_runtime_data")
         local link = assert(find_file("prelink.exe", {"$(env PATH)"}), "Cannot find prelink.exe")
 
-        table.join({path.absolute(vformat("$(buildir)/.prelink")), path.absolute(data)}, args)
+        args = table.join({vformat("$(buildir)/.prelink"), path.absolute(data)}, args)
         os.runv(link, args)
         target:add("linkdirs", "$(buildir)/.prelink/lib")
         target:add("shflags", "bedrock_server_api.lib", {force = true})
