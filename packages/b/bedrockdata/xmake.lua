97d13f284f09a9fd29c2190852d65370d89b2996
OEOTYAN <OEOTYAN@outlook.com>
2024-08-08 04:36:43 +0800

    repo(bedrockdata): update version

--- packages/b/bedrockdata/xmake.lua
+++ packages/b/bedrockdata/xmake.lua
@@ -3,6 +3,7 @@ package("bedrockdata")
     add_urls("https://github.com/LiteLDev/bedrock-runtime-data/releases/download/$(version)/bedrock-runtime-data-$(version)-bdswin.zip")
 
     add_versions("1.21.0.03", "54ecd5130b6aeec9971bc5f5c9b7cc82a9aecc21b4745c193bc7b78d8eeaf78c")
+    add_versions("1.21.3.01", "ee8a2abc48b4cdeed9761adc3cbbe741b3224e5b2ed7c61a45cee3ae2d4750fe")
 
     add_deps("prelink")
 
