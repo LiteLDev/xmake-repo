ddf737710dfe19874844c17f921d36260fe716ad
OEOTYAN <OEOTYAN@outlook.com>
2024-08-13 06:27:52 +0800

    fix: fix std binding

--- packages/b/bedrockdata/xmake.lua
+++ packages/b/bedrockdata/xmake.lua
@@ -11,7 +11,7 @@ package("bedrockdata")
         end})
         add_versions("1.21.3-01-server", "ee8a2abc48b4cdeed9761adc3cbbe741b3224e5b2ed7c61a45cee3ae2d4750fe")
         add_versions("1.21.0-03-server", "54ecd5130b6aeec9971bc5f5c9b7cc82a9aecc21b4745c193bc7b78d8eeaf78c")
-        add_versions("1.21.0-03-client", "5643160ab6f83e5c1c2c7aede7afa447cc44cc65d3903a104e544775f9861890")
+        add_versions("1.21.0-03-client", "5185c203b8f705e3d8a181008363638e3194d9e826bdd4d7b98a68a53c7f32cc")
     end
 
     add_deps("prelink")
