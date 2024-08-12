98e53394b180749c9c9f3460a2188fbfd525f91a
OEOTYAN <OEOTYAN@outlook.com>
2024-08-12 17:52:09 +0800

    repo: update versions

--- packages/b/bedrockdata/xmake.lua
+++ packages/b/bedrockdata/xmake.lua
@@ -1,13 +1,18 @@
 package("bedrockdata")
     set_kind("binary")
-    add_urls("https://github.com/LiteLDev/bedrock-runtime-data/releases/download/$(version)/bedrock-runtime-data-$(version)-bdswin.zip")
 
-    add_versions("1.21.0.03", "54ecd5130b6aeec9971bc5f5c9b7cc82a9aecc21b4745c193bc7b78d8eeaf78c")
-    add_versions("1.21.3.01", "ee8a2abc48b4cdeed9761adc3cbbe741b3224e5b2ed7c61a45cee3ae2d4750fe")
+    on_source(function (package)
+        if package:is_plat("windows") and package:is_arch("x64") then
+            package:set("urls", "https://github.com/LiteLDev/bedrock-runtime-data/releases/download/$(version)/bedrock-runtime-data-$(version)-windows-x64.zip")
+            package:add("versions", "1.21.0.03-server", "54ecd5130b6aeec9971bc5f5c9b7cc82a9aecc21b4745c193bc7b78d8eeaf78c")
+            package:add("versions", "1.21.3.01-server", "ee8a2abc48b4cdeed9761adc3cbbe741b3224e5b2ed7c61a45cee3ae2d4750fe")
+            package:add("versions", "1.21.0.03-client", "c5f623d6b0aee79d530421d99654bca9537224ac178b6ccb27fc63e59c0df2c0")
+        end
+    end)
 
     add_deps("prelink")
 
-    on_install("windows", function (package)
+    on_install(function (package)
         os.mv("*", package:installdir("bin"))
         package:addenv("PATH", "bin")
     end)
--- packages/p/prelink/xmake.lua
+++ packages/p/prelink/xmake.lua
@@ -1,10 +1,15 @@
 package("prelink")
     set_kind("binary")
-    add_urls("https://github.com/LiteLDev/prelink-release/releases/download/$(version)/prelink-$(version)-windows-amd64.zip")
 
-    add_versions("0.1.0", "8edd959195a9a95cb0545acbe725c15dc5dbb4675df03486b62119f0e5b9b9e6")
+    on_source(function (package)
+        if package:is_plat("windows") and package:is_arch("x64") then
+            package:set("urls", "https://github.com/LiteLDev/prelink-release/releases/download/$(version)/prelink-$(version)-windows-x64.zip")
+            package:add("versions", "0.1.0", "8edd959195a9a95cb0545acbe725c15dc5dbb4675df03486b62119f0e5b9b9e6")
+            package:add("versions", "0.2.0", "f28c22455953201ce2233d4d9eed2c3ab8cd64b82cc241604231d78b793e5b7e")
+        end
+    end)
 
-    on_install("windows", function (package)
+    on_install(function (package)
         os.mv("*", package:installdir("bin"))
         package:addenv("PATH", "bin")
     end)
