55e11399e2bc6598dce211b0516bc2833aa8fc80
OEOTYAN <OEOTYAN@outlook.com>
2024-08-13 00:06:55 +0800

    fix: fix client export

--- packages/b/bedrockdata/xmake.lua
+++ packages/b/bedrockdata/xmake.lua
@@ -11,7 +11,7 @@ package("bedrockdata")
         end})
         add_versions("1.21.3-01-server", "ee8a2abc48b4cdeed9761adc3cbbe741b3224e5b2ed7c61a45cee3ae2d4750fe")
         add_versions("1.21.0-03-server", "54ecd5130b6aeec9971bc5f5c9b7cc82a9aecc21b4745c193bc7b78d8eeaf78c")
-        add_versions("1.21.0-03-client", "2c90fea2657c6b9c9d9fea7344db22195f696abbfcc14858642dedaddc424fc6")
+        add_versions("1.21.0-03-client", "5643160ab6f83e5c1c2c7aede7afa447cc44cc65d3903a104e544775f9861890")
     end
 
     add_deps("prelink")
