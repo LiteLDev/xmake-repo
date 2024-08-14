11d17199d3758bfed2baecd798dd22265f8cac3c
OEOTYAN <OEOTYAN@outlook.com>
2024-08-14 20:23:27 +0800

    repo(bedrockdata): rename url

--- packages/b/bedrockdata/xmake.lua
+++ packages/b/bedrockdata/xmake.lua
@@ -2,16 +2,10 @@ package("bedrockdata")
     set_kind("binary")
 
     if is_plat("windows") and is_arch("x64") then
-        set_urls("https://github.com/LiteLDev/bedrock-runtime-data/releases/download/$(version)-windows-x64.zip",
-        {version = function (version)
-            local verstr = tostring(version)
-            local mcver, side = verstr:match("(.*)-(.*)")
-            mcver = mcver:gsub("-", ".")
-            return mcver.."/bedrock-runtime-data-"..side
-        end})
-        add_versions("1.21.3-01-server", "ee8a2abc48b4cdeed9761adc3cbbe741b3224e5b2ed7c61a45cee3ae2d4750fe")
-        add_versions("1.21.0-03-server", "54ecd5130b6aeec9971bc5f5c9b7cc82a9aecc21b4745c193bc7b78d8eeaf78c")
-        add_versions("1.21.0-03-client", "4f64f17ead168c3f4bffbeb7668bc76ad85c2611426590a75967e9a589113b2c")
+        set_urls("https://github.com/LiteLDev/bedrock-runtime-data/releases/download/$(version)/bedrock-runtime-data-windows-x64.zip")
+        add_versions("1.21.3.1-server", "ee8a2abc48b4cdeed9761adc3cbbe741b3224e5b2ed7c61a45cee3ae2d4750fe")
+        add_versions("1.21.0.3-server", "54ecd5130b6aeec9971bc5f5c9b7cc82a9aecc21b4745c193bc7b78d8eeaf78c")
+        add_versions("1.21.0.3-client", "4f64f17ead168c3f4bffbeb7668bc76ad85c2611426590a75967e9a589113b2c")
     end
 
     add_deps("prelink")
