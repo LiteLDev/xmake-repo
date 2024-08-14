6bdfb15e661eed44177500edcab3c9b62fdb1668
OEOTYAN <OEOTYAN@outlook.com>
2024-08-14 15:42:50 +0800

    refactor: change bedrockdata release name

--- packages/b/bedrockdata/xmake.lua
+++ packages/b/bedrockdata/xmake.lua
@@ -7,7 +7,7 @@ package("bedrockdata")
             local verstr = tostring(version)
             local mcver, side = verstr:match("(.*)-(.*)")
             mcver = mcver:gsub("-", ".")
-            return mcver.."/bedrock-runtime-data-"..mcver.."-"..side
+            return mcver.."/bedrock-runtime-data-"..side
         end})
         add_versions("1.21.3-01-server", "ee8a2abc48b4cdeed9761adc3cbbe741b3224e5b2ed7c61a45cee3ae2d4750fe")
         add_versions("1.21.0-03-server", "54ecd5130b6aeec9971bc5f5c9b7cc82a9aecc21b4745c193bc7b78d8eeaf78c")
