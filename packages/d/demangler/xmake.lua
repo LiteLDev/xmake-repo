1b6a6f0110edd6ee962599097a0f710cfe13a042
OEOTYAN <OEOTYAN@outlook.com>
2024-10-15 00:10:44 +0800

    repo(demangler): update version

--- packages/d/demangler/xmake.lua
+++ packages/d/demangler/xmake.lua
@@ -8,6 +8,7 @@ package("demangler")
     add_versions("v17.0.6", "94d8cd7a51ddd432e56ca1aba0fc091a48a10dff")
     add_versions("v17.0.7", "efd63f3c976493f42766edfbcc1642a0062d8517")
     add_versions("v19.1.1", "5656d832cb64a4274018d75ea79905dc5d5da301")
+    add_versions("v19.1.2", "d82dd687fafb5e70f22598423345800182c754ff")
 
     on_install(function (package)
         local configs = {}
