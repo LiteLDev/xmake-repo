db183ae86505c48d4d349835f535333865ab9f93
ShrBox <shrbox@outlook.com>
2025-01-03 17:19:53 +0800

    repo(quickjs): add 2024-02-14

--- packages/q/quickjs/xmake.lua
+++ packages/q/quickjs/xmake.lua
@@ -1,6 +1,7 @@
 package("quickjs")
     add_urls("https://github.com/LiteLDev/quickjs/releases/download/$(version)/quickjs-win-x86_64.zip")
     add_versions("2022-03-07", "af0c38b0cf80aa1deb58e727e408477fffcc6f5f57da537dffc335861d652ed0")
+    add_versions("2024-02-14", "a180e3601d5ee1d28cf1d876d7529a00437504f0b39e1b31ec858cd6e497d81d")
 
     on_install(function (package)
         os.cp("*", package:installdir())
