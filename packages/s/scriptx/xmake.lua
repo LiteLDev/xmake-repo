818739278f54c7177336ba055e2ba54fa7663b0d
ShrBox <shrbox@outlook.com>
2025-01-03 21:42:47 +0800

    repo(scriptx): fix quickjs-ng config

--- packages/s/scriptx/xmake.lua
+++ packages/s/scriptx/xmake.lua
@@ -29,4 +29,8 @@ package("scriptx")
         package:add("defines", "SCRIPTX_BACKEND=" .. backend)
         package:add("defines", "SCRIPTX_BACKEND_TRAIT_PREFIX=../backend/" .. backend .. "/trait/Trait")
         package:add("deps", deps[backend])
+        if (backend == "QuickJs") then
+            local qjsng = package:dep("quickjs-ng")
+            qjsng:config_set("libc", true)
+        end
     end)
