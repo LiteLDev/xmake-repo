191c565094d737f03b6a0d6512d1940814ed92e7
OEOTYAN <OEOTYAN@outlook.com>
2025-01-06 05:27:13 +0800

    fix: fix some error

--- packages/m/moodycamelconqueue/xmake.lua
+++ packages/m/moodycamelconqueue/xmake.lua
@@ -8,10 +8,17 @@ package("moodycamelconqueue")
 
     add_versions("v1.0.4", "87fbc9884d60d0d4bf3462c18f4c0ee0a9311d0519341cac7cbd361c885e5281")
 
+    add_deps("cmake")
+
+    add_includedirs("include", "include/concurrentqueue")
+
     on_load(function (package)
         package:set("kind", "library", {headeronly = true})
     end)
 
     on_install(function (package)
-        os.cp("include/concurrentqueue/moodycamel/*", package:installdir("include", "moodycamel"))
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:is_debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
     end)
