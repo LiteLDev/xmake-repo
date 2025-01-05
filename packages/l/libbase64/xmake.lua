fd4a9b13cb1f6f4e4e2c2e950573e47bbe49c930
OEOTYAN <OEOTYAN@outlook.com>
2025-01-06 04:28:23 +0800

    repo: add libbase64

---
+++ packages/l/libbase64/xmake.lua
@@ -0,0 +1,16 @@
+package("libbase64")
+    set_description("Fast Base64 stream encoder/decoder in C99, with SIMD acceleration")
+
+    set_urls("https://github.com/aklomp/base64.git")
+
+    add_versions("0.5.2", "8bdda2d47caf8b066999c5bd01069e55bcd0d396")
+
+    add_deps("cmake")
+
+    on_install(function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+        os.mv("include/*", package:installdir("include"))
+    end)
