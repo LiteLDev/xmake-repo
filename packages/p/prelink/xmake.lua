d2485a97d154703a57abefacb4ba99891338be94
Dofes <91889957+Dofes@users.noreply.github.com>
2025-01-05 16:22:49 +0800

    repo(prelink): add 0.3.0

--- packages/p/prelink/xmake.lua
+++ packages/p/prelink/xmake.lua
@@ -5,6 +5,7 @@ package("prelink")
        set_urls("https://github.com/LiteLDev/prelink-release/releases/download/$(version)/prelink-$(version)-windows-x64.zip")
        add_versions("0.1.0", "8edd959195a9a95cb0545acbe725c15dc5dbb4675df03486b62119f0e5b9b9e6")
        add_versions("0.2.0", "5fee60e94a7f557bcb818842d37091e1f766aa8ffeefd5be1895b02e030b5653")
+       add_versions("0.3.0", "1a4a17b1acb56f4d1aa5098dc9457d30a02ca9fe6f15d986db10288bede2870e")
     end
 
     on_install(function (package)
