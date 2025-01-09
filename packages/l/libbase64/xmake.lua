b1d63a36be9f3c4bdc1167ed500c6437114f08d6
OEOTYAN <OEOTYAN@outlook.com>
2025-01-10 01:04:37 +0800

    repo(libbase64): add some config

--- packages/l/libbase64/xmake.lua
+++ packages/l/libbase64/xmake.lua
@@ -7,12 +7,16 @@ package("libbase64")
 
     add_deps("cmake")
 
-    add_defines("BASE64_STATIC_DEFINE")
+    on_load(function(package)
+        if not package:config("shared") then
+            package:add("defines", "BASE64_STATIC_DEFINE")
+        end
+    end)
 
     on_install(function (package)
         local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=Release")
-        table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
         import("package.tools.cmake").install(package, configs)
         os.mv("include/*", package:installdir("include"))
         os.mv("lib/*.lib", package:installdir("lib"))
