6d9c144bd490a2da06f4f9019f5df700ed8d2fa4
ShrBox <shrbox@outlook.com>
2025-02-03 19:51:28 +0800

    repo(lightwebsocketclient): add v1.0.1

--- packages/l/lightwebsocketclient/xmake.lua
+++ packages/l/lightwebsocketclient/xmake.lua
@@ -3,6 +3,7 @@ package("lightwebsocketclient")
 
     add_urls("https://github.com/LiteLDev/LightWebSocketClient.git")
     add_versions("v1.0.0", "31a1a9c430894fc43b07190fd62f06c143f81fbf")
+    add_versions("v1.0.1", "d6798453c5f0d8a9ac1b261c2ea03e8b4cbaab78")
 
     on_install(function (package)
         local configs = {}
