02523ba1a34710c33a5ba77bb3532d230a29dc2f
RimuruChan <RealSprite233@outlook.com>
2025-02-09 21:25:23 +0800

    repo(pe_bliss): update to v1.0.1

--- packages/p/pe_bliss/xmake.lua
+++ packages/p/pe_bliss/xmake.lua
@@ -3,6 +3,7 @@ package("pe_bliss")
 
     add_urls("https://github.com/LiteLDev/pe_bliss.git")
     add_versions("v1.0.0", "8b7712b719275a94732045e0d6d381e33f4db536")
+    add_versions("v1.0.1", "bbd242facd06f52f5249301ff2e686a46540ffce")
 
     on_install(function (package)
         local configs = {}
