3ad4a82283e69136a5448c058a5688ad7062da3b
ShrBox <shrbox@outlook.com>
2025-04-20 21:32:13 +0800

    repo(trampoline): try fix install

--- packages/t/trampoline/xmake.lua
+++ packages/t/trampoline/xmake.lua
@@ -5,7 +5,7 @@ package("trampoline")
 
     add_versions("2024.11.7", "e6abf9e4dba627052c8222825569d55831cb9f40")
 
-    add_patches("2024.11.7", "patches/patch-cmake-version.diff", "b23a32267c2616ac2e204da0dcb95c091165631af87b69d78caeeb12d4a381b6")
+    add_patches("2024.11.7", "patches/patch-cmake-version.diff", "f8a4e5dcbfb520f836d5328f8c56bf29de6a6e90fa983b2b6766b9c67eab897c")
 
     add_deps("cmake")
 
