cc16047967a0844988f09cbf78aab24e8e97f3da
ShrBox <shrbox@outlook.com>
2025-04-20 21:45:41 +0800

    repo(trampoline): fix checksum

--- packages/t/trampoline/xmake.lua
+++ packages/t/trampoline/xmake.lua
@@ -5,7 +5,7 @@ package("trampoline")
 
     add_versions("2024.11.7", "e6abf9e4dba627052c8222825569d55831cb9f40")
 
-    add_patches("2024.11.7", "patches/patch-cmake-version.diff", "f8a4e5dcbfb520f836d5328f8c56bf29de6a6e90fa983b2b6766b9c67eab897c")
+    add_patches("2024.11.7", "patches/patch-cmake-version.diff", "41227ec63675efa2896a3db0f21a1a61785a49a14d284ee8abeee6bd98870367")
 
     add_deps("cmake")
 
