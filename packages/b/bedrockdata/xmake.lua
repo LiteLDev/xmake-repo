eb668ec42bb41f6b159c00a54bacd35fa543a42a
ShrBox <shrbox@outlook.com>
2025-05-06 13:24:46 +0800

    repo(bedrockdata): prepare for 1.21.70

--- packages/b/bedrockdata/xmake.lua
+++ packages/b/bedrockdata/xmake.lua
@@ -11,7 +11,26 @@ package("bedrockdata")
         add_versions("1.21.60.10-server", "64b0117b7bedd5c5fc718cc30d7e045ccf4103e0a2c40008695d935f6e3fba82")
     end
 
-    add_deps("prelink")
+    local deps = {
+        ["1.21.0"] = {"prelink 0.3.0"},
+        ["1.21.3"] = {"prelink 0.3.0"},
+        ["1.21.20"] = {"prelink 0.3.0"},
+        ["1.21.50"] = {"prelink 0.3.0"},
+        ["1.21.60"] = {"prelink 0.3.0"},
+        ["1.21.70"] = {"prelink 0.4.0"}
+    }
+
+    on_load(function(package)
+        local version = package:version_str()
+        local major, minor, patch = version:match("(%d+)%.(%d+)%.(%d+)")
+        if not major or not minor or not patch then
+            return
+        end
+        local dep = deps[major .. "." .. minor .. "." .. patch]
+        if dep then
+            package:add("deps", dep)
+        end
+    end)
 
     on_install(function (package)
         os.cp("*", package:installdir("bin"))
