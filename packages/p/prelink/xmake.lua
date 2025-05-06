9e2060a9d42511809904b5074eeae7ac3c081fef
ShrBox <shrbox@outlook.com>
2025-05-06 20:31:28 +0800

    repo: update preloader and prelink

--- packages/p/prelink/xmake.lua
+++ packages/p/prelink/xmake.lua
@@ -6,6 +6,7 @@ package("prelink")
        add_versions("0.1.0", "8edd959195a9a95cb0545acbe725c15dc5dbb4675df03486b62119f0e5b9b9e6")
        add_versions("0.2.0", "5fee60e94a7f557bcb818842d37091e1f766aa8ffeefd5be1895b02e030b5653")
        add_versions("0.3.0", "1a4a17b1acb56f4d1aa5098dc9457d30a02ca9fe6f15d986db10288bede2870e")
+       add_versions("0.4.0", "5f47ef425452c03ea0c6db0dcf130cb66d8f15c4288c57ff42bbec086a4d8262")
     end
 
     on_install(function (package)
