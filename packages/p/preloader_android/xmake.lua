02e62b234a0c8d407d104fb6559ebd68ab221dd7
GX <guxiang@litebds.com>
2025-05-08 20:06:51 +0800

    repo(preloader_android): update to 0.1.2

--- packages/p/preloader_android/xmake.lua
+++ packages/p/preloader_android/xmake.lua
@@ -7,6 +7,7 @@ package("preloader_android")
 
     add_versions("0.1.0", "68917bd2b699d40827a39c616ac8bda367da6c9e1e98977fc80cb4f85256506e")
     add_versions("0.1.1", "aaecf8869083132cdf20effe6add3ca19f82b5bc6af83472b6e6b5426c3b6d5e")
+    add_versions("0.1.2", "a355798b397e7f4642c7994003fb84f69300c35261209f35d6fe7f92ac654d20")
 
     add_configs("shared", {description = "Build shared library.", default = true, type = "boolean"})
 
