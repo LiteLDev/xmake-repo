2e1ad57d0ca20aff9d269abadbf6f45636440403
RimuruChan <RealSprite233@outlook.com>
2025-05-17 23:05:09 +0800

    repo(bedrockdata): bump to v1.21.7004-server.6

--- packages/b/bedrockdata/xmake.lua
+++ packages/b/bedrockdata/xmake.lua
@@ -3,6 +3,7 @@ package("bedrockdata")
 
     if is_plat("windows") and is_arch("x64") then
         set_urls("https://github.com/LiteLDev/bedrock-runtime-data/releases/download/$(version)/bedrock-runtime-data-windows-x64.zip")
+        set_urls("https://github.com/LiteLDev/bedrock-runtime-data/releases/download/$(version)/bedrock-runtime-data-$(version)-windows-x64.zip")
         add_versions("1.21.0.3-server", "54ecd5130b6aeec9971bc5f5c9b7cc82a9aecc21b4745c193bc7b78d8eeaf78c")
         add_versions("1.21.0.3-client", "c7e41b174226e7dcb7a10ff0e3cd1c48628e217133e5d02b8eee89ab045384c6")
         add_versions("1.21.3.1-server", "995cdb5f111aae059f9036410ab3cb283d0be915404662584cc87700cb383259")
@@ -14,25 +15,27 @@ package("bedrockdata")
         add_versions("v1.21.7004-server.3", "903e6d6d0e74f9ba956dccb591610de49d18371e304fecfddb8c84add3c1db7a")
         add_versions("v1.21.7004-server.4", "903e6d6d0e74f9ba956dccb591610de49d18371e304fecfddb8c84add3c1db7a")
         add_versions("v1.21.7004-server.5", "e355e3336c136235741530e502dc4fff81577d2ca2e5841f1304b16966744c94")
+        add_versions("v1.21.7004-server.6", "be354c7d6368abdab1fb425f9389993bfeb0d0c68328d8525147a453fb5907d2")
     end
 
     local deps = {
-        ["1.21.0"] = {"prelink 0.3.0"},
-        ["1.21.3"] = {"prelink 0.3.0"},
-        ["1.21.20"] = {"prelink 0.3.0"},
-        ["1.21.50"] = {"prelink 0.3.0"},
-        ["1.21.60"] = {"prelink 0.3.0"},
-        ["1.21.70"] = {"prelink 0.4.1"},
-        ["1.21.7004"] = {"prelink 0.4.1"}
+        ["1.21.0.3-server"] = {"prelink v0.3.0"},
+        ["1.21.0.3-client"] = {"prelink v0.3.0"},
+        ["1.21.3.1-server"] = {"prelink v0.3.0"},
+        ["1.21.20.3-server"] = {"prelink v0.3.0"},
+        ["1.21.50.10-server"] = {"prelink v0.3.0"},
+        ["1.21.60.10-server"] = {"prelink v0.3.0"},
+        ["1.21.70.04-server.1"] = {"prelink v0.4.1"},
+        ["1.21.70.04-server.2"] = {"prelink v0.4.1"},
+        ["v1.21.7004-server.3"] = {"prelink v0.4.1"},
+        ["v1.21.7004-server.4"] = {"prelink v0.4.1"},
+        ["v1.21.7004-server.5"] = {"prelink v0.4.1"},
+        ["v1.21.7004-server.6"] = {"prelink v0.5.0"},
     }
 
     on_load(function(package)
-        local version = package:version_str()
-        local major, minor, patch = version:match("v?(%d+)%.(%d+)%.(%d+)")
-        if not major or not minor or not patch then
-            return
-        end
-        local dep = deps[major .. "." .. minor .. "." .. patch]
+        local version = tostring(package:version_str())
+        local dep = deps[version]
         if dep then
             package:add("deps", dep)
         end
