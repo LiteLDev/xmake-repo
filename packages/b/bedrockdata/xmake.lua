d473945f3cc4ba4de8c3df94f880d78f52759170
ShrBox <shrbox@outlook.com>
2025-07-23 16:14:18 +0800

    repo(bedrockdata): add v1.21.93-server.4

--- packages/b/bedrockdata/xmake.lua
+++ packages/b/bedrockdata/xmake.lua
@@ -32,6 +32,7 @@ package("bedrockdata")
         ["v1.21.93-server.1"] = {"prelink v0.5.0"},
         ["v1.21.93-server.2"] = {"prelink v0.5.0"},
         ["v1.21.93-server.3"] = {"prelink v0.5.0"},
+        ["v1.21.93-server.4"] = {"prelink v0.5.0"},
     }
 
     on_load(function(package)
