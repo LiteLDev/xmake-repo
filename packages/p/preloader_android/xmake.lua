9c9b23968d24e3e9a178fd6dfdf99c1a7a339f1b
Esczvefs <guxiang@litebds.com>
2025-10-11 14:28:20 +0800

    repo(preloader_android): update to 0.1.8

--- packages/p/preloader_android/xmake.lua
+++ packages/p/preloader_android/xmake.lua
@@ -1,20 +1,25 @@
 package("preloader_android")
     set_homepage("https://github.com/LiteLDev/preloader-android")
     set_description("The preloader package")
+    set_kind("library")
+    add_urls("https://github.com/LiteLDev/preloader-android/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/LiteLDev/preloader-android.git")
 
-    add_urls("https://github.com/LiteLDev/preloader-android/archive/refs/tags/$(version).tar.gz")
-    add_urls("https://github.com/LiteLDev/preloader-android.git")
+    add_versions("0.1.8", "2661091a8a7b44d11887c21efe4e75a60e474e7f086f261bbe71f302cbb419a8")
 
-    add_versions("0.1.0", "68917bd2b699d40827a39c616ac8bda367da6c9e1e98977fc80cb4f85256506e")
-    add_versions("0.1.1", "aaecf8869083132cdf20effe6add3ca19f82b5bc6af83472b6e6b5426c3b6d5e")
-    add_versions("0.1.2", "a355798b397e7f4642c7994003fb84f69300c35261209f35d6fe7f92ac654d20")
+    add_deps("cmake")
 
-    add_configs("shared", {description = "Build shared library.", default = true, type = "boolean"})
+    on_load(function (package)
+        package:data_set("cmake.build_shared", true)
+    end)
 
-    on_install(function(package)
-        local configs = {}
-        if package:config("shared") then
-            configs.kind = "shared"
+    on_install(function (package)
+        import("package.tools.cmake").install(package, {"-DBUILD_SHARED_LIBS=ON"})
+        local instdir = package:installdir()
+        if os.isdir("include") then
+            os.cp("include/*", path.join(instdir, "include"))
         end
-        import("package.tools.xmake").install(package, configs)
+        if os.isdir("lib") then
+            os.cp("lib/*", path.join(instdir, "lib"))
+        end
     end)
