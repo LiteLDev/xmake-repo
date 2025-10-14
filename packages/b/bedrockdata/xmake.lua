75dcbc92d5cf08564c5603b357ea74178808c0bf
ShrBox <shrbox@outlook.com>
2025-10-14 21:25:22 +0800

    repo(bedrockdata): bump to v1.21.111-server.3

--- packages/b/bedrockdata/xmake.lua
+++ packages/b/bedrockdata/xmake.lua
@@ -39,6 +39,7 @@ package("bedrockdata")
         ["v1.21.102-server.2"] = {"prelink v0.7.0"},
         ["v1.21.111-server.1"] = {"prelink v0.7.0"},
         ["v1.21.111-server.2"] = {"prelink v0.7.0"},
+        ["v1.21.111-server.3"] = {"prelink v0.7.0"},
     }
 
     on_load(function(package)
