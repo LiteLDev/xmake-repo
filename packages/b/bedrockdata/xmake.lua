6bf0585f2a813a2cbbf44b44e1189d2db0b091a4
ShrBox <shrbox@outlook.com>
2025-12-07 00:32:44 +0800

    repo(bedrockdata): add alias to separate old and new url

--- packages/b/bedrockdata/xmake.lua
+++ packages/b/bedrockdata/xmake.lua
@@ -1,33 +1,32 @@
 package("bedrockdata")
 
     if is_plat("windows") and is_arch("x64") then
-        add_urls("https://github.com/LiteLDev/bedrock-runtime-data/releases/download/$(version)/bedrock-runtime-data-$(version)-windows-x64.zip")
-        add_urls("https://github.com/LiteLDev/bedrock-runtime-data/releases/download/$(version)/bedrock-runtime-data-windows-x64.zip")
+        add_urls("https://github.com/LiteLDev/bedrock-runtime-data/releases/download/$(version)/bedrock-runtime-data-$(version)-windows-x64.zip", {alias = "new"})
+        add_urls("https://github.com/LiteLDev/bedrock-runtime-data/releases/download/$(version)/bedrock-runtime-data-windows-x64.zip", {alias = "old"})
         add_versionfiles("versions.txt")
     end
 
     local deps = {
-        ["1.21.0.3-server"] = {"prelink v0.3.0"},
-        ["1.21.0.3-client"] = {"prelink v0.3.0"},
-        ["1.21.3.1-server"] = {"prelink v0.3.0"},
-        ["1.21.20.3-server"] = {"prelink v0.3.0"},
-        ["1.21.50.10-server"] = {"prelink v0.3.0"},
-        ["1.21.60.10-server"] = {"prelink v0.3.0"},
-        ["1.21.70.04-server"] = {"prelink v0.4.1"},
-        ["v1.21.7004-server.3"] = {"prelink v0.4.1"},
-        ["v1.21.7004-server.4"] = {"prelink v0.4.1"},
-        ["v1.21.7004-server.5"] = {"prelink v0.4.1"},
-        ["v1.21.7004-server.6"] = {"prelink v0.5.0"},
-        ["v1.21.7004-server.7"] = {"prelink v0.5.0"},
-        ["v1.21.7004-server.9"] = {"prelink v0.5.0"},
-        ["v1.21.803-server"] = {"prelink v0.5.0"},
-        ["v1.21.80-server"] = {"prelink v0.5.0"},
-        ["v1.21.93-server"] = {"prelink v0.5.0"},
-        ["v1.21.102-server"] = {"prelink v0.7.0"},
-        ["v1.21.111-server"] = {"prelink v0.7.0"},
-        ["v1.21.120-server"] = {"prelink v0.7.0"},
-        ["v1.21.124-server"] = {"prelink v0.7.0"},
-
+        ["old:1.21.0.3-server"] = {"prelink v0.3.0"},
+        ["old:1.21.0.3-client"] = {"prelink v0.3.0"},
+        ["old:1.21.3.1-server"] = {"prelink v0.3.0"},
+        ["old:1.21.20.3-server"] = {"prelink v0.3.0"},
+        ["old:1.21.50.10-server"] = {"prelink v0.3.0"},
+        ["old:1.21.60.10-server"] = {"prelink v0.3.0"},
+        ["old:1.21.70.04-server"] = {"prelink v0.4.1"},
+        ["old:v1.21.7004-server.3"] = {"prelink v0.4.1"},
+        ["old:v1.21.7004-server.4"] = {"prelink v0.4.1"},
+        ["old:v1.21.7004-server.5"] = {"prelink v0.4.1"},
+        ["new:v1.21.7004-server.6"] = {"prelink v0.5.0"},
+        ["new:v1.21.7004-server.7"] = {"prelink v0.5.0"},
+        ["new:v1.21.7004-server.9"] = {"prelink v0.5.0"},
+        ["new:v1.21.803-server"] = {"prelink v0.5.0"},
+        ["new:v1.21.80-server"] = {"prelink v0.5.0"},
+        ["new:v1.21.93-server"] = {"prelink v0.5.0"},
+        ["new:v1.21.102-server"] = {"prelink v0.7.0"},
+        ["new:v1.21.111-server"] = {"prelink v0.7.0"},
+        ["new:v1.21.120-server"] = {"prelink v0.7.0"},
+        ["new:v1.21.124-server"] = {"prelink v0.7.0"},
     }
 
     on_load(function(package)
