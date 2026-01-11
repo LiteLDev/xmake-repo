a12e46526f1de2a5078059e1610a260a3e0746aa
ShrBox <shrbox@outlook.com>
2026-01-11 21:16:53 +0800

    repo(scriptx): add 2026.1.10

--- packages/s/scriptx/xmake.lua
+++ packages/s/scriptx/xmake.lua
@@ -1,6 +1,9 @@
 package("scriptx")
     add_urls("https://github.com/LiteLDev/ScriptX.git")
-    add_versions("3.2.0", "1fae62097926328b7dd7e5415344e9d7fda465b4")
+    add_versions("2024.2.9", "7376cfeadc07f6d51d4e416bda5ee14305bc8d16")
+    add_versions("2024.5.8", "006d6de1422d62aa55daef7d50367620fc661677")
+    add_versions("2025.5.22", "22184aeecdcee2c683ad99db6362837e4ec6b7fe")
+    add_versions("2026.1.11", "95c7a9fb63f62444c1f884a56e912b6e1a593ca4")
 
     add_configs("backend", {default = "Lua", values = {"Lua", "QuickJs", "Python", "V8"}})
     add_includedirs(
@@ -16,13 +19,30 @@ package("scriptx")
 
     on_load(function (package)
         local backend = package:config("backend")
-
-        local deps = {
-            Lua = "lua v5.4.7",
-            QuickJs = "quickjs-ng v0.8.0",
-            Python = "python 3.12.8",
-            V8 = "node v22.12.0",
-        }
+        local version = package:version_str()
+        local deps = {}
+        if version == "2026.1.11" then
+            deps = {
+                Lua = "lua v5.4.7",
+                QuickJs = "quickjs-ng v0.11.0",
+                Python = "python 3.12.10",
+                V8 = "node v22.12.0",
+            }
+        elseif version == "2025.5.22" or version == "22184aee" or version == "775b9f5d" then
+            deps = {
+                Lua = "lua 5.4.7",
+                QuickJs = "quickjs-ng 0.8.0",
+                Python = "python 3.12.8",
+                V8 = "node v22.12.0",
+            }
+        else
+            deps = {
+                Lua = "lua 5.4.6",
+                QuickJs = "quickjs 2024.01.13",
+                Python = "python 3.10.11",
+                V8 = "node v16.16.0",
+            }
+        end
 
         print("Using ScriptX config: backend=" .. backend)
     
