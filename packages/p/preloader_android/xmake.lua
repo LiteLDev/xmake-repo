026cb8319f1134dfd5a8f135e7d238d2e390c475
Esczvefs <guxiang@litebds.com>
2026-03-29 13:32:49 +0800

    repo(preloader_android): update to 0.1.18

--- packages/p/preloader_android/xmake.lua
+++ packages/p/preloader_android/xmake.lua
@@ -4,7 +4,8 @@ package("preloader_android")
     set_kind("library")
     add_urls("https://github.com/LiteLDev/preloader-android/archive/refs/tags/$(version).tar.gz",
              "https://github.com/LiteLDev/preloader-android.git")
-
+             
+    add_versions("0.1.18", "b93e6f042d5963c9be7a3fac52f45add65a72559052086c52d77468ca83eb700")
     add_versions("0.1.16", "87cfed2b0b5eedaa5d3fa62be61243e271fa20742419a8fd23acb7730883e692")
     add_versions("0.1.14", "fc7efef29682657ec0be725f035b0492196278ea90c4daf337f907903d0c4324")
     add_versions("0.1.13", "2b22f5260b172a7b79e3b9db5c5827a9cb4646809424d9414fd36d34c14859b3")
