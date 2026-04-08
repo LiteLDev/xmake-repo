677f50e6fd64a50f62a3b61bfd563d07947f6e49
ShrBox <shrbox@outlook.com>
2026-04-08 17:43:04 +0800

    repo(bedrockdata): add versions

--- packages/l/levilamina/versions/main.lua
+++ packages/l/levilamina/versions/main.lua
@@ -17,10 +17,10 @@ local deps_common = {
     "stb 2025.03.14"
 }
 local deps_server = {
-    "bedrockdata v26.10.4-server.4"
+    "bedrockdata v26.10.4-server.5"
 }
 local deps_client = {
-    "bedrockdata v26.10.4-client.4"
+    "bedrockdata v26.10.4-client.5"
 }
 
 function load(package)
