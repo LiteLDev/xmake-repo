79f41ba128a6dc023e78a31c2e4976606908ec81
ShrBox <shrbox@outlook.com>
2026-09-06 17:07:48 +0800

    repo(legacymoney): add v0.20.0 repo(legacyremotecall): add v0.20.0

--- packages/l/legacymoney/xmake.lua
+++ packages/l/legacymoney/xmake.lua
@@ -15,6 +15,7 @@ package("legacymoney")
     add_versions("git:0.18.0", "07c13e5329aac2c20a4d3ceed8abeb5d8c036384")
     add_versions("git:0.18.1", "4b274550398b7e6ed630953273ae455f0642c257")
     add_versions("git:0.19.0", "6269976c70d121b579f798c16391ac42a211405c")
+    add_versions("git:0.20.0", "50c043ef9940937a99e5d86a48f12ac470d793e7")
 
     add_configs("target_type", {default = "server", values = {"server", "client"}})
 
